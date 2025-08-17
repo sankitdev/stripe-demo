502a9c1fce81209f74b7c4109feedc8d4f1ee494
Ankit Singh <ankitsingh635101@gmail.com>
2025-08-17 16:01:58 +0530

    feat: setup the webhook locally successfully

--- backend/index.ts
+++ backend/index.ts
@@ -1,13 +1,20 @@
 import express from "express";
 import { dbConnect } from "./config/db";
+import WebhookRouter from "./routes/webhook";
 
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
+app.use("/", WebhookRouter);
+
+app.get("/test", (req, res) => {
+  res.send("Welcome backend is running fine.");
+});
+
+app.listen(port, async () => {
   try {
-    dbConnect();
-    console.log(`Listening on port, ${port}`);
+    await dbConnect();
+    console.log(`Listening on port ${port}`);
   } catch (error) {
     console.log("Error while connecting", error);
     process.exit(1);
